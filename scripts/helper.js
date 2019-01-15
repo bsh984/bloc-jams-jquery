53dba0000234f33effe83363ef71ff2a665363bd
Bells <bellen.shen.designs@gmail.com>
2019-01-14 23:22:51 -0500

    checks out. assignment + extra credit completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,18 +2,9 @@ class Helper{
   playPauseAndUpdate(song) {
     player.playPause(song);
 
-<<<<<<< Updated upstream
-    setInterval( () => {
-      const duration1 = player.getDuration();
-
-      $('#time-control .total-time').text(duration1);
-    });
-=======
     const durationOfSong = player.currentlyPlaying.duration;
     $('#time-control .total-time').text(player.prettyTime(durationOfSong));
->>>>>>> Stashed changes
   }
-
 }
 
 const helper = new Helper();
